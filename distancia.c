c411ee4fc9e5d417c0480c2d22bfff663fbb4b01
Mateus-SBatista <94994088+Mateus-SBatista@users.noreply.github.com>
2023-10-10 12:05:21 -0300

    removendo duplicacao

--- distancia.c
+++ distancia.c
@@ -8,7 +8,7 @@ float calculaDistancia(int x1, int y1, int x2, int y2){
 // main
 int main(){
 	
-	//definição das variaveis
+	//definicao das variaveis
 	int x1,x2,y1,y2;
 	float distancia;
 	
@@ -23,25 +23,4 @@ int main(){
 	
 	//mostra o resultado
 	printf("Distancia: %f", distancia);
-}
-#include <stdio.h>
-#include <math.h>
-
-// funcao que calcula a distancia
-float calculaDistancia(int x1, int y1, int x2, int y2){
-	return sqrt( pow(x2-x1,2)+ pow(y2-y1,2));
-}
-// main
-int main(){
-	
-	//definição das variaveis
-	int x1,x2,y1,y2;
-	float distancia;
-	
-	// leitura das coordenadas dos pontos
-	printf("Informe as coordenadas para o primeiro ponto: ");
-	scanf("%i %i", &x1, &y1);
-	printf("Informe as coordenadas para o segundo ponto: ");
-	scanf("%i %i", &x2, &y2);
-	
 }
